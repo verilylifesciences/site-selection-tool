4a2c668e82322f8c80141b6e1a08db64dec22868
Anton Geraschenko <geraschenko@gmail.com>
2021-05-27 12:08:00 -0700

    Update demo trial dates.
    In the trial specification demo, I've also truncated ground truth at
    2021-05-09. Previously the demo used as much ground truth as was
    available, but used a fixed trial interval and forecasts from a fixed
    date. This led to issue #34 when ground truth covered the entirety of
    the trial interval.

--- bsst/ville_config.py
+++ bsst/ville_config.py
@@ -14,5 +14,5 @@ FIRST_PLOT_DAY = None
 SAVE_FILE_PATH = os.path.join(os.path.abspath(__file__).split('py')[0], 'act_villes')
 FILE_OPEN_FN = bsst_io.open_fn
 
-SUCCESS_DATES = slice(np.datetime64('2020-12-01'), np.datetime64('2021-02-28'))
+SUCCESS_DATES = slice(np.datetime64('2021-07-01'), np.datetime64('2021-10-01'))
 HIST_BIN_WIDTH = 4 # time units
